df9eb4521a07bec73ef9b6664c6d3f24157be6d4
BLACKMAMBA2408 <parsaalireza26@gmail.com>
2025-05-05 09:19:45 +0330

    ninth commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gulspee9fi8"
+    "revision": "0.2hs4m3n98qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
